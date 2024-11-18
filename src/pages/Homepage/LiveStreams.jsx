3439b7bf123021a9e51fa38079aff8c8f5491316
HaiTrieu <trieuothse173617@fpt.edu.vn>
2024-11-18 09:48:27 +0700

    update ava

--- src/pages/Homepage/LiveStreams.jsx
+++ src/pages/Homepage/LiveStreams.jsx
@@ -2,19 +2,20 @@ import React from "react";
 
 const LiveStreams = () => {
   const liveStreams = [
-    { name: "John Doe", avatar: "https://via.placeholder.com/48" },
-    { name: "Jessica", avatar: "https://via.placeholder.com/48" },
-    { name: "Edututs+", avatar: "https://via.placeholder.com/48" },
-    { name: "Joginder Singh", avatar: "https://via.placeholder.com/48" },
-    { name: "Zoena", avatar: "https://via.placeholder.com/48" },
-    { name: "Albert Dua", avatar: "https://via.placeholder.com/48" },
-    { name: "Ridhima", avatar: "https://via.placeholder.com/48" },
-    { name: "Amritpal", avatar: "https://via.placeholder.com/48" },
-    { name: "Jimmy", avatar: "https://via.placeholder.com/48" },
-    { name: "Quinton Batchelor", avatar: "https://via.placeholder.com/48" },
-    { name: "Eli Natoli", avatar: "https://via.placeholder.com/48" },
-    { name: "Jaysen Batchelor", avatar: "https://via.placeholder.com/48" },
+    { name: "John Doe", avatar: "https://media.istockphoto.com/id/1299037952/vi/vec-to/bi%E1%BB%83u-t%C6%B0%E1%BB%A3ng-t%C3%A0i-kho%E1%BA%A3n-ng%C6%B0%E1%BB%9Di-d%C3%B9ng.jpg?s=1024x1024&w=is&k=20&c=YsQJH1bABPEExvmSz8b-TAD8vEKa967G7KZN1cuXsFw=" },
+    { name: "Jessica", avatar: "https://media.istockphoto.com/id/970441006/vi/vec-to/bi%E1%BB%83u-t%C6%B0%E1%BB%A3ng-avatar-nam-%C4%91%E1%BA%A7y-m%C3%A0u-s%E1%BA%AFc-v%E1%BB%9Bi-k%C3%ADnh-m%E1%BA%B7t-tr%E1%BB%9Di-tr%C3%AAn-m%E1%BA%AFt.jpg?s=1024x1024&w=is&k=20&c=vDvyhVise4YPMIS-yWnl_NqTregyDXi-wzGdLJm1Crs=" },
+    { name: "Edututs+", avatar: "https://media.istockphoto.com/id/1191084604/vi/vec-to/avatar-nam-tr%C3%AAn-n%E1%BB%81n-tr%E1%BA%AFng-bi%E1%BB%83u-t%C6%B0%E1%BB%A3ng-ng%C6%B0%E1%BB%9Di-d%C3%B9ng-h%C3%ACnh-minh-h%E1%BB%8Da-vector.jpg?s=1024x1024&w=is&k=20&c=QMs_4d4tVWm0nnMvBt3iSFphDPFVajJ5KaFTnMxfnAM=" },
+    { name: "Joginder Singh", avatar: "https://media.istockphoto.com/id/1191084605/vi/vec-to/avatar-nam-tr%C3%AAn-n%E1%BB%81n-tr%E1%BA%AFng-bi%E1%BB%83u-t%C6%B0%E1%BB%A3ng-ng%C6%B0%E1%BB%9Di-d%C3%B9ng-h%C3%ACnh-minh-h%E1%BB%8Da-vector.jpg?s=1024x1024&w=is&k=20&c=Iz168WmG4ZWSJ_1zOYrOYVESPS3QiR_JsXcmtl-OGzQ=" },
+    { name: "Zoena", avatar: "https://media.istockphoto.com/id/1020538890/vi/vec-to/d%E1%BA%A5u-hi%E1%BB%87u-vector-bi%E1%BB%83u-t%C6%B0%E1%BB%A3ng-hipster-v%C3%A0-bi%E1%BB%83u-t%C6%B0%E1%BB%A3ng-%C4%91%C6%B0%E1%BB%A3c-c%C3%A1ch-ly-tr%C3%AAn-n%E1%BB%81n-tr%E1%BA%AFng-kh%C3%A1i-ni%E1%BB%87m-bi%E1%BB%83u.jpg?s=1024x1024&w=is&k=20&c=ymq-EDjVNnEsz06E2rbzq2yUUulKjWhViplgHrRDKKE=" },
+    { name: "Albert Dua", avatar: "https://media.istockphoto.com/id/1170110277/vi/vec-to/bi%E1%BB%83u-t%C6%B0%E1%BB%A3ng-avatar-t%C3%A0i-kho%E1%BA%A3n-ng%C6%B0%E1%BB%9Di-d%C3%B9ng-ph%C6%B0%C6%A1ng-ti%E1%BB%87n-truy%E1%BB%81n-th%C3%B4ng-x%C3%A3-h%E1%BB%99i.jpg?s=1024x1024&w=is&k=20&c=DLHtyWvn5atIOatzkYrK3_bXeBaEtg33rv2M5kCk0us=" },
+    { name: "Ridhima", avatar: "https://media.istockphoto.com/id/1170110564/vi/vec-to/bi%E1%BB%83u-t%C6%B0%E1%BB%A3ng-avatar-t%C3%A0i-kho%E1%BA%A3n-ng%C6%B0%E1%BB%9Di-d%C3%B9ng-ph%C6%B0%C6%A1ng-ti%E1%BB%87n-truy%E1%BB%81n-th%C3%B4ng-x%C3%A3-h%E1%BB%99i.jpg?s=1024x1024&w=is&k=20&c=FeYQL3P0TC0-Gx1cTKQyzSJmqX48Mj9yDe5mte4oSSY=" },
+    { name: "Amritpal", avatar: "https://media.istockphoto.com/id/1170110496/vi/vec-to/bi%E1%BB%83u-t%C6%B0%E1%BB%A3ng-avatar-t%C3%A0i-kho%E1%BA%A3n-ng%C6%B0%E1%BB%9Di-d%C3%B9ng-ph%C6%B0%C6%A1ng-ti%E1%BB%87n-truy%E1%BB%81n-th%C3%B4ng-x%C3%A3-h%E1%BB%99i.jpg?s=1024x1024&w=is&k=20&c=tpvn02E9yz8WK3tki8-eq6GNPPTIY7KyJH7LEx-JRbc=" },
+    { name: "Jimmy", avatar: "https://media.istockphoto.com/id/1170110861/vi/vec-to/bi%E1%BB%83u-t%C6%B0%E1%BB%A3ng-avatar-t%C3%A0i-kho%E1%BA%A3n-ng%C6%B0%E1%BB%9Di-d%C3%B9ng-ph%C6%B0%C6%A1ng-ti%E1%BB%87n-truy%E1%BB%81n-th%C3%B4ng-x%C3%A3-h%E1%BB%99i.jpg?s=1024x1024&w=is&k=20&c=g-XbjeHQNi6J1afAcR0tWKOJapNRQhfJMTV9_vwlvcA=" },
+    { name: "Quinton Batchelor", avatar: "https://media.istockphoto.com/id/1170110566/vi/vec-to/bi%E1%BB%83u-t%C6%B0%E1%BB%A3ng-avatar-t%C3%A0i-kho%E1%BA%A3n-ng%C6%B0%E1%BB%9Di-d%C3%B9ng-ph%C6%B0%C6%A1ng-ti%E1%BB%87n-truy%E1%BB%81n-th%C3%B4ng-x%C3%A3-h%E1%BB%99i.jpg?s=1024x1024&w=is&k=20&c=8Cw-NIxJhLAbmtakYyFwPmNkHrIv7kv_8WjpltrTIMU=" },
+    { name: "Eli Natoli", avatar: "https://media.istockphoto.com/id/1169683248/vi/vec-to/bi%E1%BB%83u-t%C6%B0%E1%BB%A3ng-avatar-t%C3%A0i-kho%E1%BA%A3n-ng%C6%B0%E1%BB%9Di-d%C3%B9ng-ph%C6%B0%C6%A1ng-ti%E1%BB%87n-truy%E1%BB%81n-th%C3%B4ng-x%C3%A3-h%E1%BB%99i.jpg?s=612x612&w=0&k=20&c=nvc1CkipEPblstmM5uM9tQOvHo7EWvaHLn4VW1cgs2w=" },
+    { name: "Jaysen Batchelor", avatar: "https://media.istockphoto.com/id/1169683307/vi/vec-to/bi%E1%BB%83u-t%C6%B0%E1%BB%A3ng-avatar-t%C3%A0i-kho%E1%BA%A3n-ng%C6%B0%E1%BB%9Di-d%C3%B9ng-ph%C6%B0%C6%A1ng-ti%E1%BB%87n-truy%E1%BB%81n-th%C3%B4ng-x%C3%A3-h%E1%BB%99i.jpg?s=612x612&w=0&k=20&c=2xsEmtL1gwR6xFJnRZAdeVXMcmr4DRBsXf1KYKYn4Ys=" }
   ];
+  
 
   return (
     <div className="flex">
